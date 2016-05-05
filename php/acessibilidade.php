c82b924ea21ac8e4e79fc5759ed7c1930444a7e5
CamilaDiasOliveira <oliveiracamiladias@gmail.com>
2016-05-05 17:38:56 -0300

    pagina acessibilidade

--- php/acessibilidade.php
+++ php/acessibilidade.php
@@ -1,21 +1,22 @@
 <?php ?>
-<section class="acessibilidade secao">	
-	Acessibilidade do site
+<section class="acessibilidade secao">
+	<h1>Acessibilidade do site</h1>
+	<p> As ferramentas de acessibilidade, encontradas no topo desse site, foram desenvolvidas para atender diferentes tipos de deficiências como: baixa visão, daltonismo e mobilidade reduzida. Essas ferramentas permitem:</p>
+	
+	<ul>
+		<li>Aumentar e diminuir o tamanho das fontes;</li>
+		<li>Alterar o contraste de cores das páginas;</li>
+		<li>Acessar links de atalho ao conteúdo principal e menu;</li>
+		<li>Navegar via teclado.</li>
+	</ul>
 
-As ferramentas de acessibilidade, encontradas no topo desse site, foram desenvolvidas para atender diferentes tipos de deficiências como: baixa visão, daltonismo e mobilidade reduzida. Essas ferramentas permitem:
+	<p>Em qualquer página do site é possível acessar os conteúdos pelas teclas de atalho citadas abaixo:</p>
 
-• Aumentar e diminuir o tamanho das fontes;
-• Alterar o contraste de cores das páginas;
-• Acessar links de atalho ao conteúdo principal e menu;
-• Navegar via teclado.  
+	<p>Teclando-se ALT + 1, chega-se diretamente ao começo do conteúdo principal do site;
+	Teclando-se ALT + 2, chega-se ao início do menu principal do site, que leva a todas as páginas internas;
+	Teclando-se ALT + 3, chega-se a esta página (Acessibilidade deste site), que possui a explicação destas funções.</p>
 
-Em qualquer página do site é possível acessar os conteúdos pelas teclas de atalho citadas abaixo:
-
-Teclando-se ALT + 1, chega-se diretamente ao começo do conteúdo principal do site;
-Teclando-se ALT + 2, chega-se ao início do menu principal do site, que leva a todas as páginas internas;
-Teclando-se ALT + 3, chega-se a esta página (Acessibilidade deste site), que possui a explicação destas funções.
-
-Estes atalhos podem ser utilizados nos navegadores Internet Explorer e Chrome.
-No Firefox, no lugar de Alt + número, deve-se teclar simultaneamente Alt + Shift + número.
-No Opera, deve-se teclar Shift + Escape + número.
+	<p>Estes atalhos podem ser utilizados nos navegadores Internet Explorer e Chrome.
+	No Firefox, no lugar de Alt + número, deve-se teclar simultaneamente Alt + Shift + número.
+	No Opera, deve-se teclar Shift + Escape + número.</p>
 </section>
