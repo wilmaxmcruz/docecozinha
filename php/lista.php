4caf5bd6cc12261a5e79150649b5b143e17fa4ab
wilmaxmcruz <wilmcruz@icmc.usp.br>
2016-05-06 16:34:26 -0300

    lista php ok

--- php/lista.php
+++ php/lista.php
@@ -7,9 +7,9 @@
 				<div class="foto" style="background-image:url('images/receitas/canape-de-batata.jpg')">
 				</div>
 				<div class="infos">
-					<div>Categoria: </span><span class="categoria">Carnes</div>
+					<div>Categoria: </span><span class="categoria salgados">salgados</div>
 					<div>Publicado por: </span><span class="publicado">Doce Cozinha</div>
-					<hr class="divisoria">
+					<hr class="divisoria salgados">
 					<span class="nome">Canapé de batata</span>
 					<div class="avaliacao">
 						<div class="estrela ativa"></div>
@@ -20,11 +20,49 @@
 					</div>
 				</div>
 			</a>
-		</li>		
+		</li>
+		<li>
+			<a href="javascript:changePage('detalhes');">	
+				<div class="foto" style="background-image:url('images/receitas/vulcao-de-carne-e-bacon.jpg')">
+				</div>
+				<div class="infos">
+					<div>Categoria: </span><span class="categoria carnes">carnes</div>
+					<div>Publicado por: </span><span class="publicado">Doce Cozinha</div>
+					<hr class="divisoria carnes">
+					<span class="nome">Vulcão de bacon</span>
+					<div class="avaliacao">
+						<div class="estrela ativa"></div>
+						<div class="estrela ativa"></div>
+						<div class="estrela ativa"></div>
+						<div class="estrela ativa"></div>
+						<div class="estrela ativa"></div>
+					</div>
+				</div>
+			</a>
+		</li>	
+		<li>
+			<a href="javascript:changePage('detalhes');">	
+				<div class="foto" style="background-image:url('images/receitas/alfajor.jpg')">
+				</div>
+				<div class="infos">
+					<div>Categoria: </span><span class="categoria doces">doces</div>
+					<div>Publicado por: </span><span class="publicado">Doce Cozinha</div>
+					<hr class="divisoria doces">
+					<span class="nome">Alfajor</span>
+					<div class="avaliacao">
+						<div class="estrela ativa"></div>
+						<div class="estrela ativa"></div>
+						<div class="estrela ativa"></div>
+						<div class="estrela"></div>
+						<div class="estrela"></div>
+					</div>
+				</div>
+			</a>
+		</li>	
 	</ul>
 	<div class="mais-receitas clear">
-		<hr class="divisoria">
+		<hr class="divisoria left">
 		<button>Ver mais receitas</button>
-		<hr class="divisoria">
+		<hr class="divisoria right">
 	</div>
 </section>
