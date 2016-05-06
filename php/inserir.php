aead846a9e7b1613e7a7217491739e680f1fab70
CamilaDiasOliveira <oliveiracamiladias@gmail.com>
2016-05-06 18:36:54 -0300

    inserir formulario

--- php/inserir.php
+++ php/inserir.php
@@ -1,4 +1,33 @@
 <?php ?>
 <section class="inserir secao">	
-	Inserir Receita
+	<h1>Inserir Receita</h1>
+	<form>
+		<div>
+			<input type="text" name="autor" placeholder="Autor da receita" required>
+			<input type="text" name="titulo" placeholder="Título da receita" required>
+			<a>
+				<img src="" alt="Botão com um símbolo de adição para inserir fotos da receita"></img>
+			</a>
+		</div>
+		<div>
+			<h2>Categorias</h2>
+			<input type="checkbox" name="carnes"><label>Carnes</label>
+			<input type="checkbox" name="doces"><label>Doces</label>
+			<input type="checkbox" name="massas"><label>Massas</label>
+			<input type="checkbox" name="saladas"><label>Saladas</label>
+			<input type="checkbox" name="salgados"><label>Salgados</label>
+			<input type="checkbox" name="sopas"><label>Sopas</label>
+			
+			<div>
+				<input type="text" name="ingredientes" placeholder="Ingrediente" required>
+				<a>
+					<img src="" alt="Botão com um símbolo de adição para inserir um novo ingrediente"></img>
+				</a>
+			</div>
+			<div>
+				<input type="textarea" name="modo-preparo" placeholder="Modo de preparo" required>
+			</div>
+			<button type="button" onclick="alert('Receita salva com sucesso!')">Salvar</button>
+		</div>
+	</form>
 </section>
