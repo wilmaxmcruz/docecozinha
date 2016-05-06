38f517d8f4c58993e6c3198f5ad332873ca89182
CamilaDiasOliveira <oliveiracamiladias@gmail.com>
2016-05-06 10:22:03 -0300

    formulario login

--- php/login.php
+++ php/login.php
@@ -1,9 +1,23 @@
 <?php ?>
 <section class="login secao">	
 	<h1>Já sou cadastrado</h1>
+	
+	<div>
 	<form>
-		<input type="text" name="login"></input>
-		<input type="password" name="senha"></input>
-		<button></button>
+		<input type="text" name="login" placeholder="Login" maxlength="25" required>
+		<input type="password" name="senha" placeholder="Senha" maxlength="8" required>
+		<button type="button" onclick="alert('Login realizado com sucesso!')">Entrar</button>
 	</form>
+	</div>
+	
+	<div>
+	<h1>Quero me cadastrar</h1>
+	<form>
+		<input type="text" name="nome" placeholder="Nome completo" maxlength="50" required>
+		<input type="email" name="email" placeholder="E-mail" required>
+		
+		<h2>Preferências</h2>
+	</form>
+	</div>
+	
 </section>
