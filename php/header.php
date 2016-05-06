b83ce7effdd8783e9538cecfce419a41e908a271
CamilaDiasOliveira <oliveiracamiladias@gmail.com>
2016-05-05 22:43:28 -0300

    titulo do header para o leitor

--- php/header.php
+++ php/header.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>-.- Doce Cozinha -.-</title>
+	<title>Site Doce Cozinha</title>
 	<link rel="shortcut icon" href="css/images/favicon.ico">
 	<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,500,300italic' rel='stylesheet' type='text/css'>
 	<link href="css/reset.css" rel="stylesheet" type="text/css" />		
