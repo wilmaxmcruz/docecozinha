abd712d7def320568c20242ee286f5360ebec0ae
wilmaxmcruz <wilmcruz@icmc.usp.br>
2016-05-08 18:27:37 -0300

    inserir php v1

--- php/inserir.php
+++ php/inserir.php
@@ -2,32 +2,30 @@
 <section class="inserir secao">	
 	<h1>Inserir Receita</h1>
 	<form>
-		<div>
-			<input type="text" name="autor" placeholder="Autor da receita" required>
+		<div class="infos-receita left">
+			<input type="text" class="autor" name="autor" placeholder="Autor da receita" required>
 			<input type="text" name="titulo" placeholder="Título da receita" required>
+			<h2>Categorias</h2>
+			<div class="categorias">			
+				<input type="checkbox" name="carnes"><label>Carnes</label>
+				<input type="checkbox" name="doces"><label>Doces</label>
+				<input type="checkbox" name="massas"><label>Massas</label>
+				<input type="checkbox" name="saladas"><label>Saladas</label>
+				<input type="checkbox" name="salgados"><label>Salgados</label>
+				<input type="checkbox" name="sopas"><label>Sopas</label>
+			</div>
+			<input type="text" class="ingrediente" name="ingrediente" placeholder="Ingrediente" required>
+			<a href="javascript:void(0);">
+				<img src="images/formulario/mais-ingredientes.svg" alt="Botão com um símbolo de adição para inserir um novo ingrediente"></img>
+			</a>
+			<textarea name="modo-preparo" placeholder="Modo de preparo" required></textarea>
+		</div>
+		<div class="fotos-receita right">
+			<h2>Fotos</h2>
 			<a href="javascript:void(0);">
 				<img src="images/formulario/mais-fotos.svg" alt="Botão com um símbolo de adição para inserir fotos da receita"></img>
 			</a>
 		</div>
-		<div>
-			<h2>Categorias</h2>
-			<input type="checkbox" name="carnes"><label>Carnes</label>
-			<input type="checkbox" name="doces"><label>Doces</label>
-			<input type="checkbox" name="massas"><label>Massas</label>
-			<input type="checkbox" name="saladas"><label>Saladas</label>
-			<input type="checkbox" name="salgados"><label>Salgados</label>
-			<input type="checkbox" name="sopas"><label>Sopas</label>
-			
-			<div>
-				<input type="text" name="ingredientes" placeholder="Ingrediente" required>
-				<a href="javascript:void(0);">
-					<img src="images/formulario/mais-ingredientes.svg" alt="Botão com um símbolo de adição para inserir um novo ingrediente"></img>
-				</a>
-			</div>
-			<div>
-				<input type="textarea" name="modo-preparo" placeholder="Modo de preparo" required>
-			</div>
-			<button type="button" onclick="alert('Receita salva com sucesso!')">Salvar</button>
-		</div>
+		<button type="button" onclick="alert('Receita salva com sucesso!')">Salvar</button>
 	</form>
 </section>
