b2331aa9d654a1f3b3f35a64381d94ba316ce8fb
wilmaxmcruz <wilmcruz@icmc.usp.br>
2016-05-08 18:40:59 -0300

    inserir php ok

--- php/inserir.php
+++ php/inserir.php
@@ -3,7 +3,7 @@
 	<h1>Inserir Receita</h1>
 	<form>
 		<div class="infos-receita left">
-			<input type="text" class="autor" name="autor" placeholder="Autor da receita" required>
+			<input type="text" class="autor" name="autor" value="Autor da receita" disabled>
 			<input type="text" name="titulo" placeholder="Título da receita" required>
 			<h2>Categorias</h2>
 			<div class="categorias">			
@@ -15,17 +15,26 @@
 				<input type="checkbox" name="sopas"><label>Sopas</label>
 			</div>
 			<input type="text" class="ingrediente" name="ingrediente" placeholder="Ingrediente" required>
-			<a href="javascript:void(0);">
+			<a class="add-ingrediente" href="javascript:void(0);">
 				<img src="images/formulario/mais-ingredientes.svg" alt="Botão com um símbolo de adição para inserir um novo ingrediente"></img>
 			</a>
 			<textarea name="modo-preparo" placeholder="Modo de preparo" required></textarea>
 		</div>
 		<div class="fotos-receita right">
 			<h2>Fotos</h2>
-			<a href="javascript:void(0);">
+			<a class="foto" href="javascript:void(0);">
+				<img src="images/formulario/mais-fotos.svg" alt="Botão com um símbolo de adição para inserir fotos da receita"></img>
+			</a>
+			<a class="foto" href="javascript:void(0);">
+				<img src="images/formulario/mais-fotos.svg" alt="Botão com um símbolo de adição para inserir fotos da receita"></img>
+			</a>
+			<a class="foto" href="javascript:void(0);">
+				<img src="images/formulario/mais-fotos.svg" alt="Botão com um símbolo de adição para inserir fotos da receita"></img>
+			</a>
+			<a class="foto" href="javascript:void(0);">
 				<img src="images/formulario/mais-fotos.svg" alt="Botão com um símbolo de adição para inserir fotos da receita"></img>
 			</a>
 		</div>
-		<button type="button" onclick="alert('Receita salva com sucesso!')">Salvar</button>
+		<button type="button" onclick="alert('Receita salva com sucesso!')">Inserir receita</button>
 	</form>
 </section>
