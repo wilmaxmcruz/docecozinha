7333317b9e0cf5ff6b11cddd264a8d753d28b72a
CamilaDiasOliveira <oliveiracamiladias@gmail.com>
2016-05-09 23:25:39 -0300

    acessibilidade.php

--- php/acessibilidade.php
+++ php/acessibilidade.php
@@ -1,28 +1,30 @@
 <?php ?>
 <section class="acessibilidade secao" itemscope itemtype="http://schema.org/Recipe">
 	<h1 itemprop="name">Acessibilidade do site</h1>
-	<p>
-		As ferramentas de acessibilidade, encontradas no topo desse site, foram desenvolvidas para atender diferentes tipos de deficiências como: baixa visão, daltonismo e mobilidade reduzida. Essas ferramentas permitem:
-	</p>
-	
-	<ul itemprop="accessibilityFeature">
-		<li>Aumentar e diminuir o tamanho das fontes;</li>
-		<li>Alterar o contraste de cores das páginas;</li>
-		<li>Acessar links de atalho ao conteúdo principal e menu;</li>
-		<li>Navegar via teclado.</li>
-	</ul>
+	<div class="acessibilidade">
+		<p>
+			As ferramentas de acessibilidade, encontradas no topo desse site, foram desenvolvidas para atender diferentes tipos de deficiências como: baixa visão, daltonismo e mobilidade reduzida. Essas ferramentas permitem:
+		</p>
+		
+		<ul itemprop="accessibilityFeature">
+			<li>Aumentar e diminuir o tamanho das fontes;</li>
+			<li>Alterar o contraste de cores das páginas;</li>
+			<li>Acessar links de atalho ao conteúdo principal e menu;</li>
+			<li>Navegar via teclado.</li>
+		</ul>
 
-	<p>Em qualquer página do site é possível acessar os conteúdos pelas teclas de atalho citadas abaixo:</p>
+		<p>Em qualquer página do site é possível acessar os conteúdos pelas teclas de atalho citadas abaixo:</p>
 
-	<p itemprop="accessibilityFeature">
-		<strong>Teclando-se ALT + 1</strong>, chega-se diretamente ao começo do conteúdo principal do site;<br>
-		<strong>Teclando-se ALT + 2</strong>, chega-se ao início do menu principal do site, que leva a todas as páginas internas;<br>
-		<strong>Teclando-se ALT + 3</strong>, chega-se a esta página (Acessibilidade deste site), que possui a explicação destas funções.
-	</p>
+		<p itemprop="accessibilityFeature">
+			<strong>Teclando-se ALT + 1</strong>, chega-se diretamente ao começo do conteúdo principal do site;<br>
+			<strong>Teclando-se ALT + 2</strong>, chega-se ao início do menu principal do site, que leva a todas as páginas internas;<br>
+			<strong>Teclando-se ALT + 3</strong>, chega-se a esta página (Acessibilidade deste site), que possui a explicação destas funções.
+		</p>
 
-	<p itemprop="accessibilityFeature">
-		Estes atalhos podem ser utilizados nos navegadores <strong>Internet Explorer</strong> e <strong>Chrome</strong>.<br>
-		No <strong>Firefox</strong>, no lugar de Alt + número, deve-se teclar simultaneamente Alt + Shift + número.<br>
-		No <strong>Opera</strong>, deve-se teclar Shift + Escape + número.
-	</p>
+		<p itemprop="accessibilityFeature">
+			Estes atalhos podem ser utilizados nos navegadores <strong>Internet Explorer</strong> e <strong>Chrome</strong>.<br>
+			No <strong>Firefox</strong>, no lugar de Alt + número, deve-se teclar simultaneamente Alt + Shift + número.<br>
+			No <strong>Opera</strong>, deve-se teclar Shift + Escape + número.
+		</p>
+	</div>
 </section>
